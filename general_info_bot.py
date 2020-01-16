b7b89c3cbb581ef9cf666bf99d80fa5ca03de3e0
Ricardo Valdovinos <RicardoValdovinos1996@gmail.com>
2020-01-15 21:28:08 -0700

    added quickstart code to bot

--- general_info_bot.py
+++ general_info_bot.py
@@ -1,5 +1,9 @@
 """Discord bot."""
 import json
+import discord
+
+
+CLIENT = discord.Client()
 
 
 def read_credentials():
@@ -9,5 +13,19 @@ def read_credentials():
     return credentials
 
 
-C = read_credentials()
-print(C)
+@CLIENT.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(CLIENT))
+
+
+@CLIENT.event
+async def on_message(message):
+    if message.author == CLIENT.user:
+        return
+
+    if message.content.startswith('!weather'):
+        msg = message.content.split(' ')
+        await message.channel.send(f'{msg[1]}')
+
+
+CLIENT.run(read_credentials()['discord_auth_token'])
