655ee9e0280a47994c67231d9bef46db0922c8f6
Ricardo Valdovinos <ricardovaldovinos1996@gmail.com>
2020-01-17 18:29:00 -0700

    working towards finishing implementation of open weather maps api

--- open_weather_map.py
+++ open_weather_map.py
@@ -23,16 +23,16 @@ class OpenWeatherMapAPI:
     If you want to use the !weather command make sure to use it as shown
     below:
         !weather <zip_code>
-        !weather <zip_code> <country_code>
+        !weather <zip_code>, <country_code>
         !weather <city_name>
-        !weather <city_name> <country_code>
+        !weather <city_name>, <country_code>
         !weather <latitude> <longitude>
 
     Examples:
         !weather 85032 
-        !weather 85032 US
+        !weather 85032, US
         !weather phoenix
-        !weather phoenix US
+        !weather phoenix, US
         !weather 33 -112 
 
     Use ISO 3166 country codes. You can find country codes here: https://www.iso.org/obp/ui/#search
--- owm_parser.py
+++ owm_parser.py
@@ -6,34 +6,28 @@ def parse_weather(cmd):
     if cmd[0] == '!weather':
         cmd.remove('!weather')
         return parse_location(cmd)
-    else:
-        syntax_error()
 
 
 def parse_location(cmd):
-    if cmd[0].isdigit():
+    if cmd[0].isdigit() or is_float(cmd[0]):
         if len(cmd) == 2:
-            if cmd[1].isdigit():
+            if cmd[1].isdigit() or is_float(cmd[1]):
+                print(cmd[1])
                 return parse_coordinates(cmd)
-            else:
-                return parse_zip_code_country_code(cmd, cmd[1])
+            print('here')
+            return parse_zip_code_country_code(cmd)
         if len(cmd) == 1:
             return parse_zip_code(cmd)
     else:
-        if len(cmd) == 2:
-            return parse_city_name_country_code(cmd)
-        else:
-            return parse_city(cmd)
+        return parse_city_name_country_code(cmd)
 
 
 def parse_zip_code(zip_code):
     return '?zip=' + zip_code
 
 
-def parse_zip_code_country_code(zip_code, country_code):
-    if country_code.isdigit():
-        syntax_error()
-    return parse_zip_code(zip_code) + ',' + country_code
+def parse_zip_code_country_code(cmd):
+    return parse_zip_code(cmd[0]) + cmd[1]
 
 
 def parse_city(city_name):
@@ -41,24 +35,37 @@ def parse_city(city_name):
     for city in city_name:
         if not city.isdigit():
             full_city_name = full_city_name + city + ' '
-        else:
-            syntax_error()
     return '?q=' + full_city_name
 
 
 def parse_city_name_country_code(cmd):
     full_city_name = ''
-    for city in city_name:
-        if not city.isdigit():
-            full_city_name = full_city_name + city + ' '
+    is_city = True
+    country_code = ''
+    for param in cmd:
+        if is_city:
+            if not param.isdigit():
+                if param.find(',') == -1:
+                    full_city_name = full_city_name + param + ' '
+                else:
+                    param.replace(',', '')
+                    full_city_name = full_city_name + param + ' '
+                    is_city = False
         else:
-            syntax_error()
+            country_code = param
+    if not is_city:
+        return '?q=' + full_city_name + country_code
+    return '?q=' + full_city_name
 
 
 def parse_coordinates(coords):
+    print('?lat=' + coords[0] + '&lon=' + coords[1])
     return '?lat=' + coords[0] + '&lon=' + coords[1]
 
 
-def syntax_error():
-    print('SYNTAX ERROR!')
-    sys.exit()
+def is_float(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
